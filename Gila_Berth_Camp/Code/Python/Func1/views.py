936814db71ac8d0548a36b92dbd6ac5148d9a136
karbmk <karthikbm01@gmail.com>
2016-09-29 21:41:36 -0700

    Add files via upload

---
+++ Gila_Berth_Camp/Code/Python/Func1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+
+
+def test(request):
+    context = ""
+    return render(request,'test.html',context)
---
+++ Gila_Berth_Camp/Code/Python/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def test(request):
+    print("")
+    return HttpResponse("CUM on software developers")
