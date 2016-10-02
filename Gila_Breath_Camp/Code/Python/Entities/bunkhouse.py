0f9528662e55448880898a36ed1071c7792549d1
Jemin <jemin272@gmail.com>
2016-10-02 16:01:39 -0700

    added an entity- bunkhouse
    Signed-off-by: Jemin <jemin272@gmail.com>

---
+++ Gila_Breath_Camp/Code/Python/Entities/bunkhouse.py
@@ -0,0 +1,36 @@
+# ===============================================================================
+#                             GILA BREATH CAMP
+#
+# ===============================================================================
+# ===============================================================================
+# FILE NAME      : bunkhouse.py
+# PURPOSE        : Reading and Writing to "User.csv"
+# AUTHOR         : JEMIN GOHIL, SOHEIL BOUZARI
+# CREATION DATE  : 02-OCT-2016
+# PENDING 		 : Transformation
+# -------------------------------------------------------------------------------
+# CHANGE HISTORY :
+# VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
+# -------------------------------------------------------------------------------
+# 1.0   	02-OCT-2016  	JEMIN GOHIL, SOHEIL   Added bunkhouse_id, bunkhouse_name
+# 
+# ===============================================================================
+
+class Bunkhouse(object):
+
+	def __init__  (self):
+		self.bunkhouse_id = 0
+		self.bunkhouse_name = ''
+
+	def setBunkhouseId(self,bunkhouse_id):
+		self.bunkhouse_id = bunkhouse_id
+
+	def getBunkhouseId(self):
+		return self.bunkhouse_id
+
+	def setBunkhouseName(self,bunkhouse_name):
+		self.bunkhouse_name = bunkhouse_name
+
+	def getBunkhouseName(self):
+		return self.bunkhouse_name
+
