9056a752efeff83e0c389e19c9dd90cfbe09023e
soheil-bouzari <karthikbm01@gmail.com>
2016-10-02 16:15:29 -0700

    Created tribe.py with its basic structure
    Signed-off-by: soheil-bouzari <karthikbm01@gmail.com>

---
+++ Gila_Breath_Camp/Code/Python/Entities/tribe.py
@@ -0,0 +1,40 @@
+# ===============================================================================
+#                             GILA BREATH CAMP
+#
+# ===============================================================================
+# ===============================================================================
+# FILE NAME      : tribe.py
+# PURPOSE        : Reading and Writing to "User.csv"
+# AUTHOR         : JEMIN GOHIL, SOHEIL BOUZARI
+# CREATION DATE  : 01-OCT-2016
+# PENDING 		 : Transformation
+# -------------------------------------------------------------------------------
+# CHANGE HISTORY :
+# VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
+# -------------------------------------------------------------------------------
+# 1.0   	01-OCT-2016  	JEMIN GOHIL    		Added user_id, user_name
+# 2.0       01-OCT-2016		SOHEIL BOUZARI      Added user_type, password
+# ===============================================================================
+
+
+class Tribe(object):
+
+	def __init__(self):	
+		self.tribe_id = 0
+		self.tribe_name = ''
+		
+
+	def setTribeId(self,tribe_id):
+		self.tribe_id = tribe_id
+
+	def getTribeId(self):
+		return self.tribe_id
+
+
+
+	def setTribeName(self,tribe_name):
+		self.user_id = tribe_name
+
+
+	def getTribeName(self):
+		return self.tribe_name
