adf5806cf1d2672388774cc1428f58bb804d91a3
Karthik <karthikbm01@gmail.com>
2016-10-01 20:23:18 -0700

    structure of common_functions defined
    Signed-off-by: Karthik <karthikbm01@gmail.com>

---
+++ Gila_Breath_Camp/Code/Python/common_functions.py
@@ -0,0 +1,27 @@
+# ===============================================================================
+#                             GILA BREATH CAMP
+#
+# ===============================================================================
+# ===============================================================================
+# FILE NAME      : common_functions.py
+# PURPOSE        : READ from or WRITE to any CSV file
+# AUTHOR         : Karthik Manjunath
+# CREATION DATE  : 01-OCT-2016
+# PENDING 		 : ALL
+# -------------------------------------------------------------------------------
+# CHANGE HISTORY :
+# VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
+# -------------------------------------------------------------------------------
+# 1.0   	01-OCT-2016  	Karthik Manjunath
+#
+# ===============================================================================
+
+class Common_Csv:
+	def insertIntoCsv():
+		pass
+	
+	def updateIntoCsv():
+		pass
+	
+	def getFromCsv():
+		pass
