25ef4e66e8047393849d537dfe0e5386835fddf1
Karthik <karthikbm01@gmail.com>
2016-10-03 01:36:49 -0700

    changed common_functions.py
    Signed-off-by: Karthik <karthikbm01@gmail.com>

---
+++ Gila_Breath_Camp/Code/Dustbin/Karthik/test.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("Python")
+sys.path.append("Python/Entities")
+import common_functions
+import user
+
+k = common_functions.Common_functions()
+'''
+ur = user.User()
+ur.setUserId("6")
+ur.setUserName("vini")
+ur.setUserType("DIRECTOR")
+ur.setPassword("ilovejemin")
+
+k.insertIntoCsv("user.csv",ur)
+'''
+a = k.getFromCsv("user.csv",{})
+print(a[0].user_name)
