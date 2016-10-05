341d0a2db6bddd3ec9830756deca06e85dfe2654
Karthik <karthikbm01@gmail.com>
2016-10-04 21:00:54 -0700

    registration UI added
    Signed-off-by: Karthik <karthikbm01@gmail.com>

--- Gila_Breath_Camp/Code/Python/views.py
+++ Gila_Breath_Camp/Code/Python/views.py
@@ -6,6 +6,12 @@ from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from django.http import HttpResponse
+import sys
+sys.path.append("Python")
+sys.path.append("Python/Entities")
+import common_functions
+import user
+import ast
 
 
 def test(request):
@@ -18,4 +24,17 @@ def create_volume(request):
 	c.update(csrf(request));
 	data = request.POST["vol_name"]
 	print("data"+data)
+	print(type(data))
+	data_dict = ast.literal_eval(data)
+	print(data_dict["password"])
+	k = common_functions.Common_functions()
+	ur = user.User()
+	ur.setUserId(data_dict["user_id"])
+	ur.setUserName(data_dict["user_name"])
+	ur.setUserType(data_dict["user_type"])
+	if ur.setPassword(data_dict["password"]) == "Password should be more than 5 characters":
+		return false
+	else:
+		ur.setPassword(data_dict["password"])
+	k.insertIntoCsv("user.csv",ur)
 	return HttpResponse("success",content_type="application/type")
--- Gila_Breath_Camp/Code/Static/js/test.js
+++ Gila_Breath_Camp/Code/Static/js/test.js
@@ -1,7 +1,24 @@
 getTest = function(id)
 { 
-	alert(document.getElementById("user_id").value);
-	k = document.getElementById("user_id").value;
+	//alert(document.getElementById("user_id").value);
+	//k = document.getElementById("user_id").value;
+    var conf_password = document.getElementById("confirm").value;
+    //alert(conf_password);
+    var k =
+        {
+            c_first_name:document.getElementById("c_first_name").value,
+            c_last_name:document.getElementById("c_last_name").value,
+            c_age:document.getElementById("c_age").value,
+            c_sex:document.getElementById("c_sex").value,
+            c_address:document.getElementById("c_address").value,
+            g_first_name:document.getElementById("g_first_name").value,
+            g_last_name:document.getElementById("g_last_name").value,
+            g_address:document.getElementById("g_address").value,
+            g_contact_info:document.getElementById("g_contact_info").value,
+            g_emergency_contact:document.getElementById("g_emergency_contact").value,
+            g_payment:document.getElementById("g_payment").value
+        }
+    var input = JSON.stringify(k)
 	$.ajax
 	(
 		{
@@ -9,18 +26,18 @@ getTest = function(id)
 			url:"../create_volume/",
 			data: 
    				{
-        			vol_name: k // from form
+        			vol_name: input // from form
         		},
     		dataType: "text",
     		success: function(data) 
     		{
         		debugger;
-        		alert("success")
+        		alert("thank you behenchod tune regester karli hain")
     		},
     		error: function(data)
     		{
         		debugger;
-        		alert("fail");          
+        		alert("chutiye password galat kiya hoga dekh le");          
       		}
     	}
   	);
