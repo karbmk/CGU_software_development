2430ef7b50b0837785fcec32db7646123aa50050
Jemin <jemin272@gmail.com>
2016-10-04 21:09:26 -0700

    completed transformation for applicant.py
    Signed-off-by: Jemin <jemin272@gmail.com>

--- Gila_Breath_Camp/Code/Python/Entities/applicant.py
+++ Gila_Breath_Camp/Code/Python/Entities/applicant.py
@@ -7,13 +7,13 @@
 # PURPOSE        : Reading and Writing to "User.csv"
 # AUTHOR         : JEMIN GOHIL, SOHEIL BOUZARI
 # CREATION DATE  : 02-OCT-2016
-# PENDING 		 : Transformation
+# PENDING 		 :
 # -------------------------------------------------------------------------------
 # CHANGE HISTORY :
 # VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
 # -------------------------------------------------------------------------------
 # 1.0   	02-OCT-2016  	JEMIN GOHIL, SOHEIL   Added all functions, testing required
-# 
+# 2.0		04-OCT-2016		JEMIN GOHIL			  Added Transformation
 # ===============================================================================
 
 class Applicant(object):
@@ -28,7 +28,7 @@ class Applicant(object):
 		self.application_date = ''
 		self.emergency_contact = ''
 		self.age = ''
-		self.sex = ''
+		self.gender = ''
 		self.address = ''
 		self.legal_form = ''
 		self.medical_form = ''
@@ -43,73 +43,109 @@ class Applicant(object):
 	def getApplicantId(self):
 		return self.applicant_id
 
+
 	def setLastName(self,last_name):
-		self.last_name = last_name.strip()
+		last_name = last_name.strip()
 		if last_name.isalpha():
-			self.last_name = last_name
+			self.last_name = last_name.upper()
 		else:
 			return "Enter only alphabets"
 
 	def getLastName(self):
 		return self.last_name
 
+
 	def setFirstName(self,first_name):
-		self.first_name = first_name.strip()
+		first_name = first_name.strip()
 		if first_name.isalpha():
-			self.first_name = first_name
+			self.first_name = first_name.upper()
 		else:
 			return "Enter only alphabets"
 
 	def getFirstName(self):
 		return self.first_name
 
+
 	def setGuardianFirstName(self,guardian_first_name):
+		guardian_first_name = guardian_first_name.strip()
 		if guardian_first_name.isalpha():
-			self.guardian_first_name = guardian_first_name
+			self.guardian_first_name = guardian_first_name.upper()
 		else:
 			return "Enter only alphabets"
 
 	def getGuardianFirstName(self):
 		return self.guardian_first_name
 
+
 	def setGuardianLastName(self,guardian_last_name):
+		guardian_last_name = guardian_last_name.strip()
 		if guardian_last_name.isalpha():
-			self.guardian_last_name = guardian_last_name
+			self.guardian_last_name = guardian_last_name.upper()
 		else:
 			return "Enter only alphabets"
 
 	def getGuardianLastName(self):
 		return self.guardian_last_name
 
+
 	def setGuardianContactNumber(self,guardian_contact_number):
-		self.guardian_contact_number = guardian_contact_number
+		guardian_contact_number = guardian_contact_number.strip()
+		if (len(guardian_contact_number) == 10 and guardian_contact_number.isdigit()):
+			self.guardian_contact_number = guardian_contact_number
+		else:
+			return "Enter 10 digits properly"
 
 	def getGuardianContactNumber(self):
 		return self.guardian_contact_number
 
+
+	from datetime import datetime
+
 	def setApplicationDate(self,application_date):
-		self.application_date = application_date
+		try: 
+			date = datetime.strptime(application_date, '%m-%d-%Y')
+			self.application_date = date
+
+		except:
+			return "Enter proper Date"
+
 
 	def getApplicationDate(self):
 		return self.application_date
 
+
 	def setEmergencyContact(self,emergency_contact):
-		self.emergency_contact = emergency_contact
+		emergency_contact = emergency_contact.strip()
+		if (len(emergency_contact) == 10 and emergency_contact.isdigit()):
+			self.emergency_contact = emergency_contact
+		else:
+			return "Enter 10 digits properly"
 
 	def getEmergencyContact(self):
 		return self.emergency_contact
 
+
 	def setAge(self,age):
-		self.age = age
+		age = age.strip()
+		if (len(age) < 4 and age.isdigit()):
+			self.age = age
+		else:
+			return "Enter proper age"
 
 	def getAge(self):
 		return self.age
 
-	def setSex(self,sex):
-		self.sex = sex
 
-	def getSex(self):
-		return self.sex
+	def setGender(self,gender):
+		gender = gender.strip()
+		if gender.isalpha():
+			self.gender = gender
+		else:
+			return "Enter proper gender"
+
+	def getGender(self):
+		return self.gender
+
 
 	def setAddress(self,address):
 		self.address = address
@@ -117,11 +153,13 @@ class Applicant(object):
 	def getAddress(self):
 		return self.address
 
+
 	def setLegalForm(self,legal_form):
 		self.legal_form = legal_form
 
 	def getLegalForm(self):
 		return self.legal_form
+
 
 	def setMedicalForm(self,medical_form):
 		self.medical_form = medical_form
@@ -129,17 +167,20 @@ class Applicant(object):
 	def getMedicalForm(self):
 		return self.medical_form
 
+
 	def setBunkhouseId(self,bunkhouse_id):
 		self.bunkhouse_id = bunkhouse_id
 
 	def getBunkhouseId(self):
 		return self.bunkhouse_id
 
+
 	def setTribeId(self,tribe_id):
 		self.tribe_id = tribe_id
 
 	def getTribeId(self):
 		return self.tribe_id
+
 
 
 	
