e6c57c8c48e2d9df366395e584c41ed5e699891c
Rohan <rohan9969@gmail.com>
2016-10-15 16:53:51 -0700

    Made Registration user story
    Signed-off-by: Rohan <rohan9969@gmail.com>

--- Gila_Breath_Camp/Code/Dustbin/Rohan/test_registration.py
+++ Gila_Breath_Camp/Code/Dustbin/Rohan/test_registration.py
@@ -4,7 +4,7 @@ import ast
 sys.path.append("Python/User_Stories")
 import registration
 
-front_end_str = json.dumps({"data" :[{
+front_end_str1 = json.dumps({"data" :[{
 "applicant_id":"",
 "user_id":"1",
 "bunkhouse_id":"",
@@ -34,5 +34,36 @@ front_end_str = json.dumps({"data" :[{
 "application_status":""
 }]})
 
-regis = registration.Registration(front_end_str)
-print(regis)
+front_end_str2 = json.dumps({"data" :[{
+"applicant_id":"",
+"user_id":"1",
+"bunkhouse_id":"",
+"tribe_id":"",
+"camp_time_slots":"2016-10-15 00:00:00.000000",
+"applicant_first_name":"212",
+"applicant_last_name":"12",
+"applicant_age":"ABC",
+"applicant_gender":"Male",
+"applicant_address":"1455 N College Ave, Claremont, CA 91711",
+"guardian_first_name":"21",
+"guardian_last_name":"212",
+"guardian_contact_number":"212",
+"guardian_address":"1455 N College Ave, Claremont, CA 91711",
+"application_date":"",
+"emergency_contact":"212",
+"payment":"212",
+"medical_form":"",
+"legal_form":"",
+"helmet":"",
+"boot":"",
+"sleeping_bag":"",
+"water_bottle":"",
+"sunscreen":"",
+"bugs_spray":"",
+"check_in_status":"",
+"application_status":""
+}]})
+
+regis = registration.Registration()
+st = regis.register(front_end_str2)
+print(st)
--- Gila_Breath_Camp/Code/Python/User_Stories/registration.py
+++ Gila_Breath_Camp/Code/Python/User_Stories/registration.py
@@ -16,34 +16,61 @@
 # ================================================================================
 
 import sys
+import json
 import ast
 import datetime
+sys.path.append("Python")
+import common_functions
 sys.path.append("Python/Entities")
 import applicant
-import user
 
 class Registration(object):
 
-	def __init__(self,front_end_str):
+	def register(self,front_end_str):
+
+		cf = common_functions.Common_functions()
 		appl = applicant.Applicant()
+
+		error = []
+		message = ''
+		none = 0
+		return_front_end_dict = ''
+
 		front_end_dict = ast.literal_eval(front_end_str)
 		front_end_data = front_end_dict['data'][0]
-		appl.setUserId(front_end_data['user_id'])
-		appl.setCampTimeSlots(front_end_data['camp_time_slots'])
-		appl.setApplicantFirstName(front_end_data['applicant_first_name'])
-		appl.setApplicantLastName(front_end_data['applicant_last_name'])
-		appl.setApplicantAge(front_end_data['applicant_age'])
-		appl.setApplicantGender(front_end_data['applicant_gender'])
-		appl.setApplicantAddress(front_end_data['applicant_address'])
-		appl.setGuardianFirstName(front_end_data['guardian_first_name'])
-		appl.setGuardianLastName(front_end_data['guardian_last_name'])
-		appl.setGuardianContactNumber(front_end_data['guardian_contact_number'])
-		appl.setGuardianAddress(front_end_data['guardian_address'])
-		appl.setApplicationDate(str(datetime.datetime.now()))
-		appl.setEmergencyContact(front_end_data['emergency_contact'])
-		appl.setPayment(front_end_data['payment'])
-		print(appl.__dict__)
+		
+		error.append(appl.setUserId(front_end_data['user_id']))
+		error.append(appl.setCampTimeSlots(front_end_data['camp_time_slots']))
+		error.append(appl.setApplicantFirstName(front_end_data['applicant_first_name']))
+		error.append(appl.setApplicantLastName(front_end_data['applicant_last_name']))
+		error.append(appl.setApplicantAge(front_end_data['applicant_age']))
+		error.append(appl.setApplicantGender(front_end_data['applicant_gender']))
+		error.append(appl.setApplicantAddress(front_end_data['applicant_address']))
+		error.append(appl.setGuardianFirstName(front_end_data['guardian_first_name']))
+		error.append(appl.setGuardianLastName(front_end_data['guardian_last_name']))
+		error.append(appl.setGuardianContactNumber(front_end_data['guardian_contact_number']))
+		error.append(appl.setGuardianAddress(front_end_data['guardian_address']))
+		error.append(appl.setApplicationDate(str(datetime.datetime.now())))
+		error.append(appl.setEmergencyContact(front_end_data['emergency_contact']))
+		error.append(appl.setPayment(front_end_data['payment']))
+		#print(front_end_str)
+		#print(error)
+
+		for i in range(0,len(error)):
+			if error[i] != None:
+				if message == '':
+					message = error[i]
+				else:
+					message = message + '|' + error[i]
+
+		if message == '':
+			cf.insertIntoCsv('applicant.csv',appl)
+			emp_appl = applicant.Applicant()
+			return_front_end_dict = '{ "data": [' + json.dumps(emp_appl.__dict__) + '], "status":"success", "message":"Registration Complete" }'
+		else:
+			return_front_end_dict = '{ "data": [' + json.dumps(appl.__dict__) + '], "status":"error", "message":"' + message + '" }'
 
 
+		return return_front_end_dict
 
 
