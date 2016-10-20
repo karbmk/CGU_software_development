253f41f29f6e032a62b53db2a052f784566751eb
Rohan <rohan9969@gmail.com>
2016-10-19 21:32:34 -0700

    Made functionality for testing registration
    Signed-off-by: Rohan <rohan9969@gmail.com>

--- Gila_Breath_Camp/Code/Testing/Test_code_user_stories/test_application_status.py
+++ Gila_Breath_Camp/Code/Testing/Test_code_user_stories/test_application_status.py
@@ -20,22 +20,46 @@ sys.path.append("Python")
 import common_functions
 sys.path.append("Python/User_Stories")
 import application_status
+sys.path.append("Testing/Test_data_user_stories")
+import csv
 
 class Test_application_status(object):
+
+	def getFromCsv(self,filename,where):
+		""" Read to csv file """
+		
+		# Reading csv and storing data in List of List
+		list_data = self.csvToListOfList('Test_data_user_stories/'+filename) 
+		# Converting List of List to List of Dictionary
+		list_dict_data = self.convertListToDict(list_data)
+		list_dict_data_where = []
+		
+		if where == {}:
+			# return all the data if there is no where clause defined
+			list_dict_data_where = list_dict_data
+		else:
+			# find rows that match the where
+			
+			for i in range(0,len(list_dict_data)):
+				dict = {}
+				for columns in where:
+					if list_dict_data[i][columns] == where[columns]:
+						list_dict_data_where.append(list_dict_data[i])
+
+		return list_dict_data_where
 
 	def test_getApplicationStatus(self):
 		""" testing function getApplicationStatus """
 		
 		appl_st = application_status.Application_status()
-		out1 = appl_st.getApplicationStatus()
 		
-		cf = common_functions.Common_functions()
+		data = self.getFromCsv('test_data_registration.csv',{})
+		return_front_end_dict = '{ "data": ' + json.dumps(data) + '}'
 		
-		list_dict = ast.literal_eval(st)
-		
-		for i range(0,len(list_dict)):
-			dict = dict['data'][0]
-	
+		for i range(0,len(data)):
+			out1 = appl_st.getApplicationStatus(return_front_end_dict)
+
+
 	def test_updateApplicationStatus(self):
 		appl_st = application_status.Application_status()
 		appl_st.updateApplicationStatus()
--- Gila_Breath_Camp/Code/Testing/Test_code_user_stories/test_registration.py
+++ Gila_Breath_Camp/Code/Testing/Test_code_user_stories/test_registration.py
@@ -4,7 +4,7 @@
 # ===============================================================================
 # ===============================================================================
 # FILE NAME      : registration.py
-# PURPOSE        : Logic for Registration
+# PURPOSE        : testing logic for Registration
 # AUTHOR         : ROHAN SAWANT
 # CREATION DATE  : 15-OCT-2016
 # PENDING 		 : 
@@ -13,64 +13,55 @@
 # VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
 # -------------------------------------------------------------------------------
 # 1.0   	01-OCT-2016  	ROHAN SAWANT    		Started coding
-# 2.0		15-OCT-2016		ROHAN SAWANT			First version of Registration User Story
 # ================================================================================
 
 import sys
-import json
-import ast
-import datetime
 sys.path.append("Python")
 import common_functions
-sys.path.append("Python/Entities")
-import applicant
+sys.path.append("Python/User_Stories")
+import registration
+sys.path.append("Testing/Test_data_user_stories")
+import csv
+import json
 
-class Registration(object):
+class Test_registration(object):
 
-	def register(self,front_end_str):
-
-		cf = common_functions.Common_functions()
-		appl = applicant.Applicant()
-
-		error = []
-		message = ''
-		none = 0
-		return_front_end_dict = ''
-
-		front_end_dict = ast.literal_eval(front_end_str)
-		front_end_data = front_end_dict['data'][0]
+	def getFromCsv(self,filename,where):
+		""" Read to csv file """
 		
-		error.append(appl.setUserId(front_end_data['user_id']))
-		error.append(appl.setCampTimeSlots(front_end_data['camp_time_slots']))
-		error.append(appl.setApplicantFirstName(front_end_data['applicant_first_name']))
-		error.append(appl.setApplicantLastName(front_end_data['applicant_last_name']))
-		error.append(appl.setApplicantAge(front_end_data['applicant_age']))
-		error.append(appl.setApplicantGender(front_end_data['applicant_gender']))
-		error.append(appl.setApplicantAddress(front_end_data['applicant_address']))
-		error.append(appl.setGuardianFirstName(front_end_data['guardian_first_name']))
-		error.append(appl.setGuardianLastName(front_end_data['guardian_last_name']))
-		error.append(appl.setGuardianContactNumber(front_end_data['guardian_contact_number']))
-		error.append(appl.setGuardianAddress(front_end_data['guardian_address']))
-		error.append(appl.setApplicationDate(str(datetime.datetime.now())))
-		error.append(appl.setEmergencyContact(front_end_data['emergency_contact']))
-		error.append(appl.setPayment(front_end_data['payment']))
-		#print(front_end_str)
-		#print(error)
-
-		for i in range(0,len(error)):
-			if error[i] != None:
-				if message == '':
-					message = error[i]
-				else:
-					message = message + '|' + error[i]
-
-		if message == '':
-			cf.insertIntoCsv('applicant.csv',appl)
-			emp_appl = applicant.Applicant()
-			return_front_end_dict = '{ "data": [' + json.dumps(emp_appl.__dict__) + '], "status":"success", "message":"REGISTRATION COMPLETE" }'
+		cf = common_functions.Common_functions()
+		# Reading csv and storing data in List of List
+		list_data = cf.csvToListOfList('Testing/Test_data_user_stories/'+filename) 
+		# Converting List of List to List of Dictionary
+		list_dict_data = cf.convertListToDict(list_data)
+		list_dict_data_where = []
+		
+		if where == {}:
+			# return all the data if there is no where clause defined
+			list_dict_data_where = list_dict_data
 		else:
-			return_front_end_dict = '{ "data": [' + json.dumps(appl.__dict__) + '], "status":"error", "message":"' + message + '" }'
+			# find rows that match the where
+			
+			for i in range(0,len(list_dict_data)):
+				dict = {}
+				for columns in where:
+					if list_dict_data[i][columns] == where[columns]:
+						list_dict_data_where.append(list_dict_data[i])
 
-		return return_front_end_dict
+		return list_dict_data_where
+
+	def test_register(self):
+	
+		reg = registration.Registration()
+				
+		data = self.getFromCsv('test_data_registration.csv',{})
+				
+		for i in range(0,len(data)):
+			return_front_end_dict = '{ "data": ' + json.dumps([data[i]]) + '}'
+		
+			reg.register(return_front_end_dict)
+		
+a = Test_registration()
+a.test_register()
 
 
