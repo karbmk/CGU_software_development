a67208751172f0db64f5aa4372add2dd1ac317c5
Jemin <jemin272@gmail.com>
2016-11-01 14:04:09 -0700

    modified user story notices
    Signed-off-by: Jemin <jemin272@gmail.com>

--- Gila_Breath_Camp/Code/Python/User_Stories/printing_of_acceptance_or_rejection_notice.py
+++ Gila_Breath_Camp/Code/Python/User_Stories/printing_of_acceptance_or_rejection_notice.py
@@ -17,26 +17,43 @@
 # ================================================================================
 
 import sys
+import ast
+import json
+sys.path.append("Python")
+import common_functions
+sys.path.append("Python/Entities")
+import applicant
+sys.path.append("Python/User_Stories")
+import application_status
 
+front_end_str = json.dumps({"data" :[{"applicant_id":"1"}]}) #Need to remove
 class Notice(object):
 	"""docstring for AcceptanceNotice"""
 	def __init__(self):
 		pass
 		
+	def getDataFromAppStatus(self,front_end_str):
+		return [{"applicant_id":"1","applicant_first_name":"ROHAN","applicant_last_name":"SAWANT","application_status":"0","violations":["Age Not in Range","Due date passed"]}]
 
-	def acceptance(self,guardian_last_name,applicant_id,applicant_first_name,camp_time_slots):
-		with open("../../Dustbin/Jemin/a_template.txt", "r") as myfile:
+	def acceptance(self,front_end_str):
+		
+		front_end_dict = ast.literal_eval(front_end_str)
+		front_end_data = front_end_dict['data'][0]
+		print('front_end_data :',front_end_data)
+
+		cf = common_functions.Common_functions() 
+		data = cf.getFromCsv('applicant.csv',front_end_data)
+		
+
+		data1 = self.getDataFromAppStatus(front_end_str)
+
+		data[0]["violations"] = data1[0]["violations"]
+		data[0]["application_status"] = data1[0]["application_status"]
+
+		
+		with open("Dustbin/Jemin/a_template.txt", "r") as myfile:
 			template = myfile.readlines()
-		
+			print(template)
 
-		template[0] = template[0].replace("*Guardian Last Name*",guardian_last_name)
-		template[1] = template[1].replace("*Applicant First Name*",applicant_first_name)
-		template[1] = template[1].replace("*Date*",camp_time_slots)
-		
-		print('\n'.join(template))
-guardian_last_name = 'Abc'
-applicant_id = '1'
-applicant_first_name = 'Def'
-camp_time_slots = 'December-2016'
-a = Notice()
-a.acceptance(guardian_last_name,applicant_id,applicant_first_name,camp_time_slots)
+ap = Notice()
+ap.acceptance(front_end_str)
