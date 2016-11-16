43ace4c107476832d3c203e16ec265ba28fe9890
rohan <rohan9969@gmail.com>
2016-11-16 23:52:06 +0530

    registration form fill

--- Gila_Breath_Camp/Testing/functional_tests/pages/home_page.py
+++ Gila_Breath_Camp/Testing/functional_tests/pages/home_page.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 from .base import Base
 from .page import Page
@@ -6,13 +8,35 @@ from .page import Page
 
 class HomePage(Page):
 
+	REGISTRATION_TAB_XPATH = "//a[@href='#tab1']"
+	CAMPER_FIRST_NAME_ID = "c_first_name"
+	CAMPER_LAST_NAME_ID = "c_last_name"
+
 	def __init__(self, browser):
 		Base.__init__(self, browser)
 
 	def visit(self):
 		self.browser.get(Base.base_url)
-		self.sleep(10)
+		self.sleep(1)
 
 	def click_on_clerk_button(self):
 		clerk_btn = self.browser.find_element_by_xpath("//a[@href='/Static/Templates/tran-his.html']")
 		webdriver.ActionChains(self.browser).move_to_element(clerk_btn).click(clerk_btn).perform()
+		self.wait_till_element_visible((By.XPATH,self.REGISTRATION_TAB_XPATH))
+
+	def enter_camper_name(self,first_name,last_name):
+		self.browser.find_element_by_id(self.CAMPER_FIRST_NAME_ID).send_keys(first_name)
+		self.browser.find_element_by_id(self.CAMPER_LAST_NAME_ID).send_keys(last_name)
+
+	def enter_campers_age(self,age):
+		self.browser.find_element_by_id("c_age").send_keys(age)
+	
+	def select_camper_gender(self,gender):
+		gender=gender.upper()		
+		select = Select(self.browser.find_element_by_id('c_sex'))
+		select.select_by_visible_text(gender)
+
+	def enter_camper_address(self,address):
+		self.browser.find_element_by_id('c_address').send_keys(address)
+
+
--- Gila_Breath_Camp/Testing/functional_tests/pages/page.py
+++ Gila_Breath_Camp/Testing/functional_tests/pages/page.py
@@ -1,6 +1,9 @@
 import random
 import time
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 class Page(object):
     def __init__(self, browser):
@@ -17,3 +20,13 @@ class Page(object):
     @staticmethod
     def sleep(seconds):
         time.sleep(seconds)
+
+    def wait_till_element_visible(self, locator, timeout=10):
+        """
+        Checks for visibility of element in screen
+        :param locator: it is touple consist of type of locator and locator
+        :param timeout: timeout for visiblity
+        :return:
+        """
+        wait = WebDriverWait(self.browser, timeout, poll_frequency=1)
+        element = wait.until(EC.visibility_of_element_located(locator))
