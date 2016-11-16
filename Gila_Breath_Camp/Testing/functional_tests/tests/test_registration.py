6e7d7bac47e541de26be06bc27b135783308ebc4
rohan <rohan9969@gmail.com>
2016-11-16 23:51:06 +0530

    registration form fill

--- Gila_Breath_Camp/Testing/functional_tests/tests/test_registration.py
+++ Gila_Breath_Camp/Testing/functional_tests/tests/test_registration.py
@@ -20,3 +20,10 @@ class TestRegistrationPage(BaseTest):
 		import pdb;pdb.set_trace()
 		#clerk_btn = driver.find_element_by_xpath("//a[@href='/Static/Templates/tran-his.html']")
 		#webdriver.ActionChains(driver).move_to_element(clerk_btn).click(clerk_btn).perform()
+
+	def test_fill_registration_form(self):
+		self.home_page.click_on_clerk_button()
+		self.home_page.enter_camper_name("Emma","watson")
+		self.home_page.enter_campers_age(27)
+		self.home_page.select_camper_gender("female")
+		self.home_page.enter_camper_address("840 Wood St, Clarion, PA 16214, USA")
