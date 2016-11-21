65197c893262421316df3a64f9b26950567404fd
Rohan <rohan9969@gmail.com>
2016-11-20 23:39:20 -0800

    Made function to set cancel flag
    Signed-off-by: Rohan <rohan9969@gmail.com>

--- Gila_Breath_Camp/Code/Python/User_Stories/application_cancellation.py
+++ Gila_Breath_Camp/Code/Python/User_Stories/application_cancellation.py
@@ -41,12 +41,48 @@ class Application_cancellation(object):
 
 		front_end_dict = ast.literal_eval(front_end_str)
 		front_end_data = front_end_dict['data'][0]
+		where = {'applicant_id': str(front_end_data['applicant_id'])}
 		
 		cf = common_functions.Common_functions()
-		data = cf.getFromCsv('applicant.csv',front_end_data)
+		data = cf.getFromCsv('applicant.csv',where)
 
-		print(data)
+		if len(data) == 0:
+			return_front_end_dict = '{ "data": "", "status":"error", "message":"Something went wrong" }'
+		else:
+			data[0]['cancel_flag'] = '1'
+			cf.updateManyRowIntoCsv('applicant.csv',data,'applicant_id')
+			return_front_end_dict = '{ "data": ' + json.dumps(data) + ', "status":"success", "message":"Applicantion has been cancelled" }'
 
+		return return_front_end_dict
+
+	def setManyCancelFlag(self,front_end_str):
+		""" set cancel at csv """
+
+		front_end_dict = ast.literal_eval(front_end_str)
+		front_end_data = front_end_dict['data']
+		
+		cf = common_functions.Common_functions()
+		data = cf.getFromCsv('applicant.csv',{})
+
+		applicant_ids = []
+		cancelled_data = []
+
+		for i in range(0,len(front_end_data)):
+			applicant_ids.append(front_end_data[i]['applicant_id'])
+
+		for j in range(0,len(data)):
+			if data[j]['applicant_id'] in applicant_ids:
+				cancelled_data.append(data[j])
+
+		if len(data) == 0:
+			return_front_end_dict = '{ "data": "", "status":"error", "message":"Something went wrong" }'
+		else:
+			for k in range(0,len(cancelled_data)):
+				cancelled_data[k]['cancel_flag'] = '1'
+			cf.updateManyRowIntoCsv('applicant.csv',cancelled_data,'applicant_id')
+			return_front_end_dict = '{ "data": "", , "status":"success", "message":"Application''s have been cancelled" }'
+
+		return return_front_end_dict
 
 
 
