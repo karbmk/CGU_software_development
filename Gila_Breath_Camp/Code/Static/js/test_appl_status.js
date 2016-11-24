4814febe3f583f78e6203b5e1a4ae8379b0f7c40
Karthik <karthikbm01@gmail.com>
2016-11-24 01:51:05 -0800

    added application guardian ssn
    Signed-off-by: Karthik <karthikbm01@gmail.com>

--- Gila_Breath_Camp/Code/Static/js/test_appl_status.js
+++ Gila_Breath_Camp/Code/Static/js/test_appl_status.js
@@ -142,7 +142,10 @@ getCancel = function(id){
 				html += '<td id="lastname'+i+'">'+obj_array[i]["applicant_last_name"]+'</td>'
 				html += '<td id="completed_appl'+i+'">'+check_stat+'</td>'
 				html += '<td align="center" id="appl_status'+i+'" name="appl_status" >'+check+'</td>'
-				html += '<td align="center"><input id="cancel'+i+'" type="checkbox" name="appl_status" '+check_cancel+'></td>'
+				if (check == 'NOT SENT')
+				{html +='<td align="center">GO TO STATUS AND SEND ACCEPTANCE PACKET</td>'}
+				else
+				{html += '<td align="center"><input id="cancel'+i+'" type="checkbox" name="appl_status" '+check_cancel+'></td>'}
 				html += '</tr>'
 				$("#cancel_status").append(html);
 				}
@@ -200,14 +203,59 @@ getPriority = function(id){
 				var html = '<tr>';
 				html += '<td id="appl'+i+'">'+obj_array[i]["applicant_id"]+'</td>'
 				html += '<td id="name'+i+'">'+obj_array[i]["applicant_name"]+'</td>'
-				html += '<td id="appl_name'+i+'"><select name="cmbCmpCrHdSal" class="form-control">'+html_appl_name+'</option></td>'
-				html += '<td id="guar_with'+i+'"><select name="cmbCmpCrHdSal" class="form-control">'+html_guar_with+'</option></td>'
+				html += '<td id="appl_name'+i+'"><select id="sel_appl_name'+i+'" onchange="next_guar_ssn(this.id)" name="cmbCmpCrHdSal" class="form-control">'+html_appl_name+'</option></td>'
+				html += '<td ><select id="guar_with'+i+'" name="cmbCmpCrHdSal" class="form-control">'+html_guar_with+'</option></td>'
 				html += '<td id="appl_name_without'+i+'"><select name="cmbCmpCrHdSal" class="form-control">'+html_appl_name_without+'</option></td>'
 				html += '<td id="guar_without'+i+'"><select name="cmbCmpCrHdSal" class="form-control">'+html_guar_without+'</option></td>'				
 				html += '</tr>'
 				$("#push_priorities").append(html);
 				}
         		
+    		},
+    		error: function(data)
+    		{
+        		alert("Sorry for the inconvinience. Server is not working. check if the server is working.");
+      		}
+    	}
+  	);
+}
+
+function next_guar_ssn(id){
+	//alert(id)
+	var e = document.getElementById(id);
+	var strUser = e.options[e.selectedIndex].value;
+	//alert(strUser)
+	appl_name = '{"data" :[{"applicant_name_together_with":"'+strUser+'"}]}'
+	$.ajax
+	(
+		{
+			type:"POST",
+			url:"../../priorities_get_guar_ssn/",
+			async:true,
+			data: 
+   				{
+        			prior: appl_name // from form
+        		},
+    		dataType: "text",
+    		success: function(data) 
+    		{
+				
+				var obj = $.parseJSON(data)
+				obj_array = obj["data"]
+				//alert(obj_array[0]["guardian_ssn_together_with"])
+				var select = document.getElementById("guar_with"+id.substr(id.length - 1));
+				var length = select.options.length;
+				for (i = 0; i < length; i++) {
+					select.options[i] = null;
+				}
+				for(i=0;i<obj_array[0]["guardian_ssn_together_with"].length;i++)
+				{
+					$("#guar_with"+id.substr(id.length - 1)).append('<option>'+obj_array[0]["guardian_ssn_together_with"][i]+'</option>');
+					//html_guar_with += html_guar_with +='<option>'+obj_array[0]["guardian_ssn_together_with"][i]+'</option>'
+				}
+				//html += '<td ><select id="guar_with'+i+'" name="cmbCmpCrHdSal" class="form-control">'+html_guar_with+'</option></td>'
+				
+				//$("#push_priorities").append(html);
     		},
     		error: function(data)
     		{
