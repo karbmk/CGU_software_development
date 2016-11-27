46df940b62cf9387c9c75518680faafac1599c6e
Rohan <rohan9969@gmail.com>
2016-11-27 00:32:36 -0800

    Made assignment of bunkhouses and tribes
    Signed-off-by: Rohan <rohan9969@gmail.com>

--- Gila_Breath_Camp/Code/Python/User_Stories/assignment_of_tribes.py
+++ Gila_Breath_Camp/Code/Python/User_Stories/assignment_of_tribes.py
@@ -3,16 +3,16 @@
 #
 # ===============================================================================
 # ===============================================================================
-# FILE NAME      : assignment_of_bunkhouses.py
-# PURPOSE        : Assign bunkhouses to applicants
+# FILE NAME      : assignment_of_tribes.py
+# PURPOSE        : Assign tribes to applicants
 # AUTHOR         : ROHAN SAWANT
-# CREATION DATE  : 26-NOV-2016
+# CREATION DATE  : 27-NOV-2016
 # PENDING 		 : 
 # -------------------------------------------------------------------------------
 # CHANGE HISTORY :
 # VER	|	DATE       	|	MODIFIED BY  		|  	CHANGE DESCRIPTION
 # -------------------------------------------------------------------------------
-# 1.0   	26-NOV-2016  	ROHAN SAWANT    		Started coding
+# 1.0   	27-NOV-2016  	ROHAN SAWANT    		Started coding
 # ===============================================================================
 
 import ast
@@ -21,17 +21,15 @@ import sys
 sys.path.append("Python")
 import common_functions
 
-class Assignment_of_bunkhouses(object):
+class Assignment_of_tribes(object):
 
-	def readBunkhouseData(self,front_end_str):
-		""" read data for Bunkhouse """
+	def readTribesData(self,front_end_str):
+		""" read data for Tribes """
 
 		front_end_dict = ast.literal_eval(front_end_str)
 		front_end_data = front_end_dict['data'][0]
 		where = [{'camp_time_slots':str(front_end_data['camp_time_slots'])}]
-		total_no_of_bunkhouses = int(front_end_data['no_of_bunkhouses'])
-		no_of_male_bunkhouses = total_no_of_bunkhouses/2
-		no_of_female_bunkhouses = total_no_of_bunkhouses/2
+		total_no_of_tribes = int(front_end_data['no_of_tribes'])
 
 		cf = common_functions.Common_functions()
 		data = cf.getAcceptedApplicants('{ "data": ' + json.dumps(where) + '}')
@@ -48,30 +46,27 @@ class Assignment_of_bunkhouses(object):
 				elif data[i]['applicant_gender'] == 'FEMALE':
 					female_data.append(data[i])
 
-			male_data_with_bunkhouses = cf.sortData(male_data,'applicant_age')
-			female_data_with_bunkhouses = cf.sortData(female_data,'applicant_age')
+			male_data_with_tribes = cf.sortData(male_data,'applicant_age')
+			female_data_with_tribes = cf.sortData(female_data,'applicant_age')
 
-			bunkhouse_id = 1
-			for k in range(0,len(male_data_with_bunkhouses)): 
-				male_data_with_bunkhouses[k]['bunkhouse_id'] = bunkhouse_id
-				bunkhouse_id += 1
-				if bunkhouse_id == no_of_male_bunkhouses + 1:
-					bunkhouse_id = 1
+			tribe_id = 1
+			for k in range(0,len(male_data_with_tribes)): 
+				male_data_with_tribes[k]['tribe_id'] = tribe_id
+				tribe_id += 1
+				if tribe_id == total_no_of_tribes + 1:
+					tribe_id = 1
 
-			bunkhouse_id = 1
-			for l in range(0,len(female_data_with_bunkhouses)): 
-				female_data_with_bunkhouses[l]['bunkhouse_id'] = bunkhouse_id
-				bunkhouse_id += 1
-				if bunkhouse_id == no_of_female_bunkhouses + 1:
-					bunkhouse_id = 1
+			for l in range(0,len(female_data_with_tribes)): 
+				female_data_with_tribes[l]['tribe_id'] = tribe_id
+				tribe_id += 1
+				if tribe_id == total_no_of_tribes + 1:
+					tribe_id = 1
 
-			all_data = male_data_with_bunkhouses + female_data_with_bunkhouses
-			cf.updateManyRowIntoCsv('applicant.csv',all_data,'applicant_id')
+			all_data = male_data_with_tribes + female_data_with_tribes
 
 			return_front_end_dict = '{ "data": ' + json.dumps(all_data) + ', "status":"success", "message":"All applicant''s information retrieved" }'
 
 		return all_data
-
 
 
 
