4cacd825820bb7bbd45190e34d8391c05ca237b3
Karthik <karthikbm01@gmail.com>
2016-12-07 00:52:41 -0800

    added update registration not in same slot
    Signed-off-by: Karthik <karthikbm01@gmail.com>

--- Gila_Breath_Camp/make_setup.py
+++ Gila_Breath_Camp/make_setup.py
@@ -278,7 +278,7 @@ def main():
 	#createSpaceInProgramFiles(text_file)
 	#file_path = input('Enter the path plus the folder name for which you need to create setup.exe :\n')
 #	file_path = "C:\Rohan\CGU\Fall 2016\Software Development\BitBucket\software_development_cgu\Gila_Breath_Camp\\folder"
-	file_path = "C:\\Users\\Acer\Desktop\CGU\SD\Bitbucket\software_development_cgu\Gila_Breath_Camp\Code"
+	file_path = "Code"
 	proj_name = 'Gila_Breath_Camp'
 	#file_path = file_path.replace("\\","\\\\")
 	#print(file_path)
