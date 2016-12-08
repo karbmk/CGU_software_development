d1f9e4aff6c6c6756ba8f616dbd4c52548486fd0
Rohan <rohan9969@gmail.com>
2016-12-08 02:53:38 -0800

    bat rename
    Signed-off-by: Rohan <rohan9969@gmail.com>

--- Gila_Breath_Camp/Continuios_integration.bat
+++
@@ -1,5 +0,0 @@
-py.test Testing/functional_tests/tests -k test_main_priority
-py.test Testing/functional_tests/tests -k test_main_check_in
-py.test Testing/functional_tests/tests -k test_main_update_register
-py.test Testing/functional_tests/tests -k test_main_cancel
-python check_for_error.py
